2556e88075dedf68aaa0a645ea98d81adcac5041
ChenLiCh <ahm12345@gmail.com>
2024-12-04 13:42:44 +0800

    add class: CProductWrap

---
+++ prjPosDemo/Models/CProductWrap.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prjPosDemo.Models
+{
+    public class CProductWrap
+    {
+        public CProductWrap(tProduct p)
+        {
+            _product = p;
+        }
+
+        private tProduct _product;
+
+        public tProduct product
+        {
+            get { return _product; }
+            set { _product = value; }
+        }
+
+        public int fId { get { return _product.fId; } set { _product.fId = value; } }
+
+        public string fName { get { return _product.fName; } set { _product.fName = value; } }
+
+        public Nullable<int> fQty { get { return _product.fQty; } set { _product.fQty = value; } }
+
+        public Nullable<decimal> fCost { get { return _product.fCost; } set { _product.fCost = value; } }
+
+        public Nullable<decimal> fPrice { get { return _product.fPrice; } set { _product.fPrice = value; } }
+
+        public string fMemo { get { return _product.fMemo; } set { _product.fMemo = value; } }
+
+        public byte[] fImage { get { return _product.fImage; } set { _product.fImage = value; } }
+    }
+}
--- prjPosDemo/tCustomer.cs
+++ prjPosDemo/tCustomer.cs
@@ -17,6 +17,10 @@ namespace prjPosDemo
     {
         public int fId { get; set; }
 
+        // [DisplayName("姓名")] 是一個屬性(Attribute)，主要用來為屬性(Property) 添加描述或顯示名稱。
+        // 於資料表格(DataGridView) 或其他 UI 控件中，屬性名稱可能會自動用作欄位標題，而 DisplayName 可以提供更友好的名稱。
+        // 這個屬性不會改變程式邏輯或資料庫結構，它純粹是為了 UI 或開發者的友好性。
+        // 對於資料庫欄位或程式碼屬性名稱，還是使用 fName，但在用戶界面上顯示的文字會是 姓名。
         [DisplayName("姓名")]
         public string fName { get; set; }
 
